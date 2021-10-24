55fed4a6f583abecc2fd38fca46de6abef3ecea9
André Aragão <andre.correia@agenciaeplus.com.br>
2021-10-23 22:16:48 -0300

    desafio concluído

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: sum_length(list, 0)
+
+  defp sum_length([], acc), do: acc
+
+  defp sum_length([_head | tail], acc) do
+    acc = acc + 1
+    sum_length(tail, acc)
   end
 end
